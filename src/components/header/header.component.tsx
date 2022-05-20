e64093d3dc67cc1b7ad7f48cf203a824b40c2df2
ShujinJS <yildirimugurfend@gmail.com>
2022-05-20 16:56:14 +0300

    style update

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -5,11 +5,12 @@ export default function Header(){
         <header id="homepageHeader">
             <div id="headerTitle"/>
             <div className="header-group">
-                <h1>
+                <h1 className="text-2xl">
                     Welcome, Pokémon trainers!
                 </h1>
                 <span>You may search for your favourites pokémons of every generations. You can study their evolutions, abilities, movesets and more!</span>
-                <span>Simply start using by typing your pokémon's name below! Happy training...</span>
+                <span><br/>Simply start using by typing your pokémon's name below!</span><br/>
+                <span>Happy training...</span>
             </div>
         </header>
     )
