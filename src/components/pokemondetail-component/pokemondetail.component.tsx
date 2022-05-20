dc45e8ec29b1c3bda139e90128aca1f9d41e308f
ShujinJS <yildirimugurfend@gmail.com>
2022-05-20 16:02:21 +0300

    pokemon detail page is better

--- src/components/pokemondetail-component/pokemondetail.component.tsx
+++ src/components/pokemondetail-component/pokemondetail.component.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 // Redux Saga
 import { useSelector } from "react-redux";
 // TS Types
@@ -9,19 +10,72 @@ export default function PokemonDetailComponent(){
 
     const myPokemon = useSelector((state: RootState) => state.rootState.selectedPokemon)
     console.log(myPokemon)
-    console.log(myPokemon.sprites)
+
+    let pokemonImage;
+ 
+    if(myPokemon.sprites!==undefined) pokemonImage = myPokemon.sprites.front_default
+
+    const pokemonStyle = {
+        height: "250px",
+        backgroundImage: `url('${pokemonImage}')`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover"
+    }
 
     return (
         <div className="pokemon-detail-container">
-            <div className="pokemon-detail-header">
-                <span>{myPokemon.name}</span>
-                <div/>
+            <div className="pokemon-detail-header pokemon-detail-row text-white">
+                <div className="flex justify-evenly">
+                    <div className="flex flex-col content-around">
+                        <div>
+                            <span className="text-xl text-white">{myPokemon.name}</span>
+                            <span className="text-emerald-100">#{myPokemon.id}</span>
+                        </div>
+                        <div className="pt-2 pb-2 font-bold">
+                            {myPokemon.types ? myPokemon.types.map((type, index) => <span key={index} className={`${type.type.name}-type pokemon-detail-type mr-2`}>{type.type.name}</span>) : ""}
+                        </div>
+                        <div className="flex flex-col">
+                            <span>Height: {myPokemon.height}</span>
+                            <span>Base Exp: {myPokemon.base_experience}</span>
+                        </div>
+                        <div>
+                            <span>Abilities:</span>
+                            <ul className="flex">
+                                {myPokemon.abilities ? myPokemon.abilities.map((ability, index) => <li key={index} className="mr-2">{ability.ability.name}</li>) : ""}
+                            </ul>
+                        </div>
+                        <div>
+                            <span className="border-y-black">Stats:</span>
+                            <div className="flex">
+                                <ul className="mr-3">
+                                    <li>hp</li>
+                                    <li>attack</li>
+                                    <li>defense</li>
+                                    <li>special attack</li>
+                                    <li>special defense</li>
+                                    <li>speed</li>
+                                </ul>
+                                <ul>
+                                    {myPokemon.stats ? myPokemon.stats.map((stat, index) => <li key={index} className={stat.base_stat < 70 ? "text-rose-400" : "text-lime-400"}>:  {stat.base_stat}</li>) : ""}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="pokemon-avatar">
+                        {myPokemon.sprites!==undefined ? 
+                        <div style={pokemonStyle}/>
+                        : <div/>}
+                    </div>
+                </div>
+                
             </div>
-            <div>
-                <span>Abilities</span>
-                <ul>
-                    <li></li>
-                </ul>
+            <div className="bg-black text-white p-4 flex justify-center">
+                <span className="pokeball-btn">Moves</span>
+            </div>
+            <div className="overflow-auto h-1/2">
+                <table className="w-full">
+                    {myPokemon.moves ? myPokemon.moves.map((move, index) => <tr><td key={index} className={index%2 == 0 ? "bg-white p-2 text-sm" : "bg-zinc-300 p-2 text-sm"}>{move.move.name}</td></tr>) : ""}
+                </table>
             </div>
         </div>
     )
