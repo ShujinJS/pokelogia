8b388415a21ce10805b41b35032817c860fcc2ec
ShujinJS <yildirimugurfend@gmail.com>
2022-05-24 15:09:36 +0300

    style update

---
+++ src/components/semi-background/semibackgorund.component.jsx
@@ -0,0 +1,6 @@
+import "../../assets/style/components-style/semibackground-style/semibackground.style.css"
+export default function SemiBackground(){
+    return(
+        <section className="semi-bg"></section>
+    )
+}
--- src/pages/PokemonDetailPage/pokemondetail.page.tsx
+++ src/pages/PokemonDetailPage/pokemondetail.page.tsx
@@ -1,11 +1,13 @@
 import PokemonDetailComponent from "../../components/pokemondetail-component/pokemondetail.component";
 // Styling
 import "../../assets/style/pages-style/pokemondetailpage-style/pokemondetailpage.style.css"
+import SemiBackground from "../../components/semi-background/semibackgorund.component";
 
 export default function PokemonDetailPage(){
 
     return (
         <section id="pokemonDetailPage">
+            <SemiBackground/>
             <PokemonDetailComponent/>
         </section>
     )
