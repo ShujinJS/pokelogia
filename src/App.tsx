abe51bc0462a8d9b9e58eced6365edb1feee73ef
ShujinJS <yildirimugurfend@gmail.com>
2022-05-24 15:45:36 +0300

    title and icon update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-
+import { useEffect } from "react"
 // Routing
 import Router from './route/route';
 // Components
@@ -7,6 +7,12 @@ import NavBar from './components/navbar/navbar.component';
 import Footer from './components/footer/footer.component';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Pokélogia | A simple pokedex app for training!";
+
+  }, []);
+
   return (
     <div className="App">
       <NavBar/>
--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -2,7 +2,7 @@ import "../../assets/style/components-style/footer-style/footer.style.css"
 
 export default function Footer(){
 
-    const icons = ["https://cdn.icon-icons.com/icons2/2415/PNG/512/react_original_logo_icon_146374.png", "https://cdn.icon-icons.com/icons2/2415/PNG/512/typescript_original_logo_icon_146317.png", "https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png", "https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg"]
+    const icons = ["https://upload.wikimedia.org/wikipedia/commons/4/47/React.svg", "https://cdn.icon-icons.com/icons2/2415/PNG/512/typescript_original_logo_icon_146317.png", "https://cdn.icon-icons.com/icons2/2415/PNG/512/redux_original_logo_icon_146365.png", "https://upload.wikimedia.org/wikipedia/commons/9/96/Sass_Logo_Color.svg"]
 
 
 
